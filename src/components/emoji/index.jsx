c991071dfd4778c865eaace270784398b30ad7fb
henryck <genapogosyan23@gmail.com>
2022-03-21 12:19:27 +0400

    fix emoji

--- src/components/emoji/index.jsx
+++ src/components/emoji/index.jsx
@@ -5,7 +5,6 @@ import styles from './styles.module.css'
 export default function Emoji({error}) {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.text}>{error ? 'За что ты так со мной?' : 'Продолжай в том же духе'}</div>
             {error ? <span className={styles.emoji}>&#128557;</span> : <span className={styles.emoji}>&#128513;</span>}
         </div>
     )
